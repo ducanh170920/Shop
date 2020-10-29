40355d95b2d10a4c7270f5a9db1ba7ef8bc04033
ducanh170920 <ducanhchelseafc@gmail.com>
2020-10-29 09:01:37 +0700

    add cart

--- store/models.py
+++ store/models.py
@@ -14,6 +14,14 @@ class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False,null=True,blank=False)
+    image = models.ImageField(null=True,blank=True)
+
+    def ImageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
     def __str__(self):
         return self.name
@@ -25,12 +33,27 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+    def get_number_item(self):
+        orderitem = self.orderitem_set.all()
+        sum = 0;
+        for item in orderitem:
+            sum+= item.quatity;
+        return sum;
+    def get_total_price_item(self):
+        orderItem = self.orderitem_set.all()
+        sum = 0
+        for item in orderItem:
+            sum+=item.total_price()
+        return sum
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True,blank=True)
     order = models.ForeignKey(Order,on_delete=models.SET_NULL,null=True,blank=True)
     quatity = models.PositiveIntegerField(default=0,null=True,blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def total_price(self):
+        return self.product.price*self.quatity
 
 class Shippingaddress(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
